0e1d5cb9dd019024ab6dbd5566e019aa72a116b2
NikaApts1auri <nika.aptsiauri77@gmail.com>
2024-08-24 19:36:51 +0400

    Wip:map data

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,15 +4,22 @@ import axios from "axios"
 
 
 export default function Product() {
-    useEffect(()=>{
-        async function getData(): Promise<void>{
-                const response=await axios.get("https://ann1.pythonanywhere.com/swagger/#/")
-         }
-         
-    })
+    useEffect(() => {
+        async function getData() {
+          try {
+            const response = await axios.get("https://ann1.pythonanywhere.com/swagger/#/");
+            setProduct(response.data);
+          } catch (error) {
+            console.error("Error fetching product data:", error);
+          }
+        }
+        getData();
+      }, []);
+    
     const [product,setProduct]=useState<object>([])
   return (
     <div className="product">
+        
     </div>
   )
 }
